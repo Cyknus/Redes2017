0ee25726b2e47941380271482e413a48a33dbfca
Alejandra <aleexkj@gmail.com>
2016-08-21 21:24:21 -0500

    labels

--- GUI/ikivy.py
+++ GUI/ikivy.py
@@ -9,6 +9,4 @@ class MyLabel(Label):
     def __init__(self, text, color):
         super(MyLabel, self).__init__(text=text, color=color)
         self.size_hint = Constants.SIZE_HINT_MSG
-        self.height = Constants.MSG_HEIGHT
-        self.width = Constants.CHAT_SIZE[0] - 10
-        # self.text_size = self.size
+        # TODO manejar el tamaño del label para que se aligne y consuma espacio necesario
