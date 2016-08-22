80605d6965c03efa2eb14a6f801bcd7aaa7ddc68
Alejandra <aleexkj@gmail.com>
2016-08-21 19:48:06 -0500

    Color & size

--- Constants/Constants.py
+++ Constants/Constants.py
@@ -1,3 +1,7 @@
 #! /usr/bin/env python3
 
 CHAT_PORT = 5000
+SIZE_LL = (500, 150)
+SIZE_RL = (500, 100)
+RGBA_BG = (.9, .9, .9, 1) #(.271, .545, .514, 1)
+CHAT_SIZE = (700, 750)
--- GUI/Chat.py
+++ GUI/Chat.py
@@ -6,13 +6,18 @@ from kivy.lang import Builder
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 
-from ApiClient import MyApiClient
+from Channel.ApiClient import MyApiClient
+from Constants import *
+
+Window.clearcolor = Constants.RGBA_BG
 
 # Descripción de las ventanas
 Builder.load_file('GUI/screens.kv')
 # Para manejar las pantallas
 sm = ScreenManager()
+
 
 class LocalLoginScreen(Screen):
     """ Clase que describe las acciones disponibles en la pantalla de acceso"""
@@ -20,11 +25,13 @@ class LocalLoginScreen(Screen):
     def accessRequest(self, my_port, contact_port):
         """ Ingreso de modo local """
         sm.current = "chat"
+        Window.size = Constants.CHAT_SIZE
 
 class RemoteLoginScreen(Screen):
     def accessRequest(self, contact_ip):
         """ Ingreso de modo remoto """
         sm.current = "chat"
+        Window.size = Constants.CHAT_SIZE
 
 class ChatScreen(Screen):
     def send(self, text): # TODO ligar con ApiClient
@@ -38,8 +45,23 @@ class ChatApp(App):
 def build_screen_manager(local):
     if local:
         sm.add_widget(LocalLoginScreen(name="login"))
+        size = Constants.SIZE_LL
     else:
         sm.add_widget(RemoteLoginScreen(name="login"))
-    sm.add_widget(ChatScreen(name="chat"))
+        size = Constants.SIZE_RL
 
-    return ChatApp()
+    # set scroll view
+    root = ChatScreen(name="chat")
+    sm.add_widget(root)
+
+    # Make sure the height is such that there is something to scroll.
+    root.ids.layout.bind(minimum_height=root.ids.layout.setter('height'))
+    # Así es como se agregan cosas..
+    for i in range(40):
+        btn = Label(text=str(i), size_hint_y=None, height=40, color=(0,0,0,1))
+        root.ids.layout.add_widget(btn)
+
+    chat = ChatApp()
+    Window.size = size
+
+    return chat
